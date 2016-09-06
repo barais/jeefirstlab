581ff12205bc4001f6b4ba6eefdd33cfd24bb032
Olivier Barais <barais@irisa.fr>
2016-09-06 16:53:34 +0200

    my first commit

---
+++ src/main/java/fr/ensai/j2eefirstlab/B.java
@@ -0,0 +1,16 @@
+package fr.ensai.j2eefirstlab;
+
+public class B {
+
+	
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
